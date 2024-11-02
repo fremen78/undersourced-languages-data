b13c842156537c2b7c7a81667de13a71bced7120
Fremen Team <fremen.git@gmail.com>
2024-11-02 19:24:35 +0000

    Added a script to convert UDHR data from xml to text

---
+++ convert-all-udhr.py
@@ -0,0 +1,19 @@
+import requests
+import xml.etree.ElementTree as ET
+
+def convert_file(lang):
+    print("PROCESSING_UDHR_LANGUAGE", lang)
+    html = None
+    url = "https://raw.githubusercontent.com/eric-muller/udhr/refs/heads/main/data/udhr/udhr_" + lang + ".xml"
+    r = requests.get(url)
+    # print len(r.content)
+    xml = r.content
+    tree = ET.fromstring(xml)
+    notags = ET.tostring(tree, encoding='utf8', method='text')
+    # print(notags.decode()[:100])
+    with open("udhr-data-text-only/udhr_" + lang + ".txt", "w") as f:
+        f.write(notags.decode())
+    return xml
+
+for lang in ["arb", "wol", "snk", "fuf", "zgh", "tzm"]:
+    convert_file(lang)
